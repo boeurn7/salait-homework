dac493b9c871b47d524aa9a700085e02a7465a4c
boeurn7 <boeurnbeng@gmail.com>
2025-12-07 12:18:21 +0700

    updated website dark mode

---
+++ BKP/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    darkMode: 'class', // important!
+    content: [
+        "./index.html",
+        "./node_modules/flowbite/**/*.js"
+    ],
+    plugins: [
+        require('flowbite/plugin')
+    ],
+}
